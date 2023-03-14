6600b83f1bec967b2ff5882a0f77e8495423081d
mulia2201010385 <127836583+mulia2201010385@users.noreply.github.com>
2023-03-14 16:56:40 +0800

    sesi1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML Sesi 1</title>
+</head>
+<body>
+    <h3>Selamat Datang <?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>ini menggunakan javascript</h3>");
+    </script>
+
+<?php
+    echo "<h3>menggunakan script PHP</h3>" ;
+?>
+
+</body>
+</html>
